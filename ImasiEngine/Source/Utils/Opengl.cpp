17c626e6fcf3268cf4269f4484852233a90d1073
Ivan Sanz <ivansanzcarasa@gmail.com>
2016-11-01 14:31:25 +0100

    [#21] Correct GL and GL_CHECK while loop
    --HG--
    branch : #21/CorrectCheckGlErrorLoop

--- ImasiEngine/Source/Utils/Opengl.cpp
+++ ImasiEngine/Source/Utils/Opengl.cpp
@@ -18,21 +18,26 @@ namespace ImasiEngine
             window->popGLStates();
         }
 
-        void checkError(const char* file, const int line, const char* call)
+        bool checkError(const char* file, const int line, const char* call)
         {
             const GLenum error = glGetError();
-            if (error != GL_NO_ERROR)
+
+            if (error == GL_NO_ERROR)
             {
-                const unsigned char* description = glewGetErrorString(error);
-                if (call != nullptr)
-                {
-                    Logger::out << "OpenGL error " << error << " `" << description << "` @ " << file << ":" << line << " - `" << call << "`" << std::endl;
-                }
-                else
-                {
-                    Logger::out << "OpenGL error " << error << " `" << description << "` @ " << file << ":" << line << std::endl;
-                }
+                return false;
             }
+
+            const unsigned char* description = glewGetErrorString(error);
+            if (call != nullptr)
+            {
+                Logger::out << "OpenGL error " << error << " `" << description << "` @ " << file << ":" << line << " - `" << call << "`" << std::endl;
+            }
+            else
+            {
+                Logger::out << "OpenGL error " << error << " `" << description << "` @ " << file << ":" << line << std::endl;
+            }
+
+            return true;
         }
     }
 }
