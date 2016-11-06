fc78d4a67a9717acecbdc436975009eccec58065
Ivan Sanz <ivansanzcarasa@gmail.com>
2016-11-06 01:16:54 +0100

    [#35] Move initBufferData to protected area
    --HG--
    branch : #35/Buffer+IndexBuffer

--- ImasiEngine/Source/Graphics/Buffers/Buffer.hpp
+++ ImasiEngine/Source/Graphics/Buffers/Buffer.hpp
@@ -16,18 +16,18 @@ namespace ImasiEngine
 
         Buffer();
 
-    public:
-
-        Buffer(const Buffer&) = delete;
-        Buffer(Buffer&& buffer) noexcept;
-        virtual ~Buffer();
-
         void initBufferData(float* data, unsigned int componentCount, unsigned int membersPerComponent);
         void initBufferData(double* data, unsigned int componentCount, unsigned int membersPerComponent);
         void initBufferData(int* data, unsigned int componentCount, unsigned int membersPerComponent);
         void initBufferData(unsigned int* data, unsigned int componentCount, unsigned int membersPerComponent);
         void initBufferData(short* data, unsigned int componentCount, unsigned int membersPerComponent);
         void initBufferData(unsigned short* data, unsigned int componentCount, unsigned int membersPerComponent);
+
+    public:
+
+        Buffer(const Buffer&) = delete;
+        Buffer(Buffer&& buffer) noexcept;
+        virtual ~Buffer();
 
         void createGLObject() override;
         void destroyGLObject() override;
