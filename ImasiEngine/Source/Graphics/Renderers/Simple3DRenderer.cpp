ea9ff37409d38817dd2281852f93db4acc895f19
Ivan Sanz <ivansanzcarasa@gmail.com>
2016-11-14 17:59:20 +0100

    [#40] Fix identation
    --HG--
    branch : #40/IntroduceRenderer

--- ImasiEngine/Source/Graphics/Renderers/Simple3DRenderer.cpp
+++ ImasiEngine/Source/Graphics/Renderers/Simple3DRenderer.cpp
@@ -9,18 +9,18 @@ namespace ImasiEngine
 
         #version 330 core
 
-            layout(location = 0) in vec3 vertexPosition_modelspace;
-            layout(location = 1) in vec2 vertexUV;
+        layout(location = 0) in vec3 vertexPosition_modelspace;
+        layout(location = 1) in vec2 vertexUV;
 
-            uniform mat4 MVP;
+        uniform mat4 MVP;
 
-            out vec2 UV;
+        out vec2 UV;
 
-            void main()
-            {
-                UV = vertexUV;
-                gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
-            }
+        void main()
+        {
+            UV = vertexUV;
+            gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+        }
 
     )SHADER_END";
 
